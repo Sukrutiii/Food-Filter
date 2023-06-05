8a09b6bad7fb4e02aebaf6cb7b4726b0500936b2
abc <abc@gmail.com>
2023-06-05 21:16:05 +0530

    first

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const foodData = 
 [
@@ -164,48 +164,53 @@ const foodData =
   }
 ]
 
-app.get('/helth-api', (req,res) => {
-    const currentTime = new Date().toLocaleTimeString();
-    const response = {
-        time: currentTime,
-        app: "express-server",
-        status: "active" 
-    };
-    res.json(response);
+// Route: /health-api
+app.get('/health-api', (req, res) => {
+  const currentTime = new Date().toLocaleTimeString();
+  const response = {
+    time: currentTime,
+    app: "express-server",
+    status: "active"
+  };
+  res.json(response);
 });
 
-app.get('/all', (req,res) =>{
-    res.json(foodData);
+// Route: /all
+app.get('/all', (req, res) => {
+  res.json(foodData);
 });
 
-app.get('/vegetable',(req,res) =>{
-    const vegetables = foodData.filter(food => food.category === 'Vegetable');
-    res.json(vegetables);
-})
+// Route: /vegetable
+app.get('/vegetable', (req, res) => {
+  const vegetables = foodData.filter(food => food.category === 'Vegetable');
+  res.json(vegetables);
+});
 
+// Route: /fruit
 app.get('/fruit', (req, res) => {
   const fruits = foodData.filter(food => food.category === 'Fruit');
   res.json(fruits);
 });
 
-
+// Route: /protein
 app.get('/protein', (req, res) => {
   const proteins = foodData.filter(food => food.category === 'Protein');
   res.json(proteins);
 });
 
-
+// Route: /calorie-above-100
 app.get('/calorie-above-100', (req, res) => {
   const above100Calories = foodData.filter(food => food.calorie > 100);
   res.json(above100Calories);
 });
 
-
+// Route: /calorie-below-100
 app.get('/calorie-below-100', (req, res) => {
   const below100Calories = foodData.filter(food => food.calorie < 100);
   res.json(below100Calories);
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
